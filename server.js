82a5cb5528d9acaef49aa2c484a2e7a100f02fac
Pratham41 <kambleprathmesh41@gmail.com>
2023-12-24 13:33:18 +0530

    add cors library

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const userRoute = require('./routes/user')
 const transactionRoute = require('./routes/transaction')
 const dotenv = require('dotenv').config();
 const path = require('path');
-
+const cors  = require("cors")
 
 const port = process.env.PORT || 8000;
 
 app.use(express.json())
+app.use(cors())
 app.use('/users',userRoute)
 app.use('/transactions',transactionRoute)
 
